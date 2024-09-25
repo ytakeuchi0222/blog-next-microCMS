41f47bcdb188207db419ed8a450f4661ff6a7dbc
ytakeuchi <hello@ytakeuchi.jp>
2024-09-25 17:58:04 +0900

    不要なコード削除

--- src/hooks/FormatDate.tsx
+++ src/hooks/FormatDate.tsx
@@ -3,9 +3,6 @@ const FormatDate = (dateTime) => {
     const year = dateTime.getFullYear();
     const month = dateTime.getMonth();
     const date = dateTime.getDate();
-    console.log(year);
-    console.log(month);
-    console.log(date);
     return `${year}/${month}/${date}`;
 };
 export default FormatDate;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,28 +4,25 @@ import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { ArticleList } from "@/components/ArticleList";
 const Home = ({ blog }) => {
-    console.log(process.env.NEXT_PUBLIC_API_KEY);
-    return (
-        <>
-            <Header></Header>
-            <div className={styles.bg}>
-                <main className={styles.main}>
-                    <ArticleList blog={blog}></ArticleList>
-                </main>
-            </div>
-            <Footer></Footer>
-        </>
-    );
+  return (
+    <>
+      <Header></Header>
+      <div className={styles.bg}>
+        <main className={styles.main}>
+          <ArticleList blog={blog}></ArticleList>
+        </main>
+      </div>
+      <Footer></Footer>
+    </>
+  );
 };
 export default Home;
-
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async () => {
-    const data = await client.get({ endpoint: "blogs" });
-
-    return {
-        props: {
-            blog: data.contents,
-        },
-    };
+  const data = await client.get({ endpoint: "blogs" });
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
 };
