1280ce9ba280ddb970e5d2b8845a53475b710f7f
ytakeuchi <hello@ytakeuchi.jp>
2024-09-27 00:42:48 +0900

    ページャーの出しわけ制御追加

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,12 +1,22 @@
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export const Pagination = ({ totalCount }) => {
-	console.log(process.env.NEXT_PUBLIC_PER_PAGE);
-	console.log(process.env.TEST);
 	const PER_PAGE_STRING: string = process.env.NEXT_PUBLIC_PER_PAGE ? process.env.NEXT_PUBLIC_PER_PAGE : '';
 	const PER_PAGE = Number(PER_PAGE_STRING);
 	const range = (start, end) => [...Array(end - start + 1)].map((_, i) => start + i);
-
+	const [isShow, setIsShow] = useState(true);
+	useEffect(() => {
+		if (totalCount / PER_PAGE > 1) {
+			setIsShow(true);
+		} else {
+			setIsShow(false);
+		}
+		// eslint-disable-next-line
+	}, []);
+	if (!isShow) {
+		return;
+	}
 	return (
 		<ul>
 			{range(1, Math.ceil(totalCount / PER_PAGE)).map((number, index) => (
