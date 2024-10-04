09f6a10352d7e6a7586d40ef7119259ac12d91e0
ytakeuchi <hello@ytakeuchi.jp>
2024-10-04 16:03:22 +0900

    リファクタリング ・CategoryとCategoryListPropsのインターフェースを追加し、型定義を行いました。 ・CategoryListコンポーネントにFC<CategoryListProps>を追加し、関数コンポーネントの引数に型を追加しました。 ・onChangeイベントハンドラを外部関数handleChangeに移動し、インライン関数を避けました。

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,34 +1,39 @@
 import Router from 'next/router';
-import { useState } from 'react';
-export const CategoryList = ({ category }) => {
+import { useState, FC } from 'react';
+
+interface Category {
+	id: string;
+	name: string;
+}
+
+interface CategoryListProps {
+	category: Category[];
+}
+
+export const CategoryList: FC<CategoryListProps> = ({ category }) => {
 	const [categoryValue, setCategoryValue] = useState('カテゴリで検索');
 
-	const categoryPush = (category) => {
+	const categoryPush = (category: string) => {
 		setCategoryValue(category);
-		if (category != 'カテゴリで検索') {
+		if (category !== 'カテゴリで検索') {
 			Router.push('/category/' + category + '?page=1');
 		}
 	};
 
+	const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		categoryPush(e.target.value);
+	};
+
 	return (
-		<>
-			{/* <ul>
-				{category.map((category) => (
-					<li key={category.id}>
-						<Link href={`/category/${category.id}?page=1`}>{category.name}</Link>
-					</li>
+		<label className="selectbox">
+			<select name="category" value={categoryValue} onChange={handleChange}>
+				<option value="カテゴリで検索">カテゴリで検索</option>
+				{category?.map((category) => (
+					<option value={category.id} key={category.id}>
+						{category.name}
+					</option>
 				))}
-			</ul> */}
-			<label className="selectbox">
-				<select name="category" value={categoryValue} onChange={(e) => categoryPush(e.target.value)}>
-					<option value="カテゴリで検索">カテゴリで検索</option>
-					{category?.map((category) => (
-						<option value={category.id} key={category.id}>
-							{category.name}
-						</option>
-					))}
-				</select>
-			</label>
-		</>
+			</select>
+		</label>
 	);
 };
